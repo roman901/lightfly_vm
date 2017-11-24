ed8b4b6611fd5aa8abae3d9efc10ae62ab2ee223
roman901 <spark@uwtech.org>
2017-11-24 11:33:59 +0300

    Add some examples and bytecode checking

--- lightfly_vm_runner/src/main.c
+++ lightfly_vm_runner/src/main.c
@@ -4,18 +4,40 @@
 
 int main(int argc, char** argv) {
     FILE *file;
+    unsigned char* program;
+    size_t length;
 
     if (argc != 2) {
         printf("Incorrect interpreter usage. Correct: lightfly_vm_runner <filename.lf>");
         return EXIT_FAILURE;
     }
 
-    // Stage 1: try to load file
-    file = fopen(argv[1], "r");
-
+    // Stage 1: try to open file
+    file = fopen(argv[1], "rb");
     if (!file) {
-        printf("Cannot open file %s", argv[1]);
+        printf("Error: cannot open file %s", argv[1]);
         return EXIT_FAILURE;
+    }
+
+    fseek (file, 0, SEEK_END);
+
+    // Stage 2: check file length
+    length = (size_t) ftell (file);
+    if (length > 65538) { // 2 for signature, 1 for version, 65535 for code
+        printf("Error: program length can't be greater than 65538 bytes (includes header)");
+    }
+
+    // Stage 3: load program to memory
+    fseek (file, 0, SEEK_SET);
+    program = malloc (length);
+    if (program) {
+        fread (program, 1, length, file);
+    }
+    fclose(file);
+
+    // Stage 4: check header signature and bytecode version
+    if (!lf_loader_check_header(program)) {
+
     }
     return EXIT_SUCCESS;
 }
