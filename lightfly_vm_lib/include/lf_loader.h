361ff31414f36c9b69ccab18d6ab00dedc354a58
roman901 <spark@uwtech.org>
2017-12-10 15:10:21 +0300

    Secure headers and make HAL

--- lightfly_vm_lib/include/lf_loader.h
+++ lightfly_vm_lib/include/lf_loader.h
@@ -1,3 +1,8 @@
+#ifndef LIGHTFLY_VM_LF_LOADER_H
+#define LIGHTFLY_VM_LF_LOADER_H
+
 int lf_loader_check_header(unsigned char* program_data);
 
 int lf_loader_check_version(unsigned char* program_data);
+
+#endif //LIGHTFLY_VM_LF_LOADER_H
